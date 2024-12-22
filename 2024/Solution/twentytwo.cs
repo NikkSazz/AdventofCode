c46a201f3a69a53d16bfacaf0179433ebe74ef07
Nikolai Sazonov <nsazonov9@gmail.com>
2024-12-22 17:34:14 -0500

    Added 22 part 1, and continuing part 2

---
+++ 2024/Solution/twentytwo.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventSazonov._2024.Solution
+{
+    internal class TwentyTwo
+    {
+        internal static void Solution(string[] input)
+        {
+            /*
+             *      While doing part one, we have the price info
+             *  from each of 2000 Secrets of every buyer
+             *  Then find top three highest prices, and thier code (four digit num)
+             *  
+             *  make get highest three value codes method
+             *  and a get code method
+             *  
+             *  then for each buyer in input
+             */
+            int generate = 10;  //  Test to 2000
+            long sum = 0;
+            List<sbyte> priceList = new List<sbyte>();
+            List<sbyte>[] priceArray = new List<sbyte>[input.Length];
+            foreach (string buyerzzz in input)
+            {
+                priceList.Clear();
+                string buyer = "123";   //  Test
+                priceList.Add(Price(buyer));
+                var test = Price(buyer);
+                long buy = long.Parse(buyer);
+                for (var i = 0; i < generate; i++)
+                {
+                    buy = Secret(buy);
+                    priceList.Add(Price(buy.ToString()));
+                }
+                Console.WriteLine($"{buyer}: {buy}");
+                sum += buy;
+                priceList.RemoveAt(priceList.Count - 1); // Remove last value, as the first one the spot
+                Console.ForegroundColor = ConsoleColor.Blue;
+                foreach (var b in priceList)
+                {
+                    Console.WriteLine("List: " + b.ToString());
+                }
+                break;  //Test
+            }
+            Console.WriteLine($"\nDay Twenty One Part One: {sum}");
+        }
+
+        static sbyte Price(string buy)
+        {
+            string buyStr = buy.ToString();
+            sbyte price = sbyte.Parse(buyStr[buyStr.Length - 1].ToString());
+            Console.WriteLine($"buyStr: {buyStr}, price: {price}");
+            return price;
+        }
+
+        static long Secret(long input)
+        {
+            long secret = input * 64;
+            secret = Mix(input, secret);
+            secret = Prune(secret);
+
+
+            input = secret;
+            secret /= 32;
+            secret = Mix(input, secret);
+            secret = Prune(secret);
+
+            input = secret;
+            secret *= 2048;
+            secret = Mix(input, secret);
+            secret = Prune(secret);
+            return secret;
+        }
+
+        static long Mix(long input, long secret)
+        {
+            return input ^ secret;
+        }
+
+        static long Prune(long input)
+        {
+            return input % 16777216;
+        }
+        /*
+         * go through each buyer and use thier 4 digit code to determine value
+         * 
+         */
+    }
+}
--- 2024/main.cs
+++ 2024/main.cs
@@ -1,4 +1,5 @@
-﻿using AdventSazonov.Solution;
+﻿using AdventSazonov._2024.Solution;
+using AdventSazonov.Solution;
 using System;
 using System.IO;
 
@@ -6,12 +7,12 @@ public class main
 {
     static void Main()
     {
-        Console.Write("Which day would you like to see: ");
+        /*Console.Write("Which day would you like to see: ");
         var request = Console.ReadLine();
-        int dayRequest = int.Parse(request);
-        //int workingOnDay = 22;
+        int dayRequest = int.Parse(request);*/
+        int workingOnDay = 22;
 
-        switch (dayRequest - 1)
+        switch (workingOnDay - 1)
         {
             case 0:
                 string[] oneInput = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"C:\Users\User\Source\Repos\Advent24Saz\2024\Inputs\one.txt"));
@@ -21,10 +22,90 @@ public class main
                 string[] twoInput = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"C:\Users\User\Source\Repos\Advent24Saz\2024\Inputs\two.txt"));
                 Two.Solution(twoInput);
                 break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                string[] nineInput = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"C:\Users\User\Source\Repos\Advent24Saz\2024\Inputs\nine.txt"));
+                string[] nineTest = { "2333133121414131402" };
+                Nine.Solution(nineInput[0]);
+                Console.WriteLine("Wrong Answer sad :(");
+                break;
+            case 9:
+                break;
+            case 10:
+                break;
+            case 11:
+                break;
+            case 12:
+                break;
+            case 13:
+                break;
+            case 14:
+                break;
+            case 15:
+                break;
+            case 16:
+                break;
+            case 17:
+                break;
+            case 18:
+                break;
+            case 19:
+                break;
+            case 20:
+                break;
+            case 21:
+                string[] twentytwoInput = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"C:\Users\User\source\repos\AdventofCode2024\twentytwo.txt"));
+                string[] ttwotest =
+                {
+                        "1",
+                        "10",
+                        "100",
+                        "2024"
+                    };
+                TwentyTwo.Solution(twentytwoInput);
+                break;
+            case 22:
+                break;
+            case 23:
+                break;
+            case 24:
+                break;
+            case 25:
+                Console.WriteLine("sbyte:\t" + sbyte.MaxValue.ToString());
+                Console.WriteLine("byte:\t" + byte.MaxValue.ToString());
+                Console.WriteLine("short:\t" + short.MaxValue.ToString());
+                Console.WriteLine("ushort:\t" + ushort.MaxValue.ToString());
+                Console.WriteLine("int:\t" + int.MaxValue.ToString());
+                Console.WriteLine("uint:\t" + uint.MaxValue.ToString());
+                Console.WriteLine("long:\t" + long.MaxValue.ToString());
+                Console.WriteLine("ulong:\t" + ulong.MaxValue.ToString());
+                break;
         }
 
         Console.WriteLine("\nPress any key twice to exit");
         Console.ReadKey();
         Console.ReadKey();
     }
+
+    /*
+        sbyte:  127
+        byte:   255
+        short:  32767
+        ushort: 65535
+        int:    2147483647
+        uint:   4294967295
+        long:   9223372036854775807
+        ulong:  18446744073709551615
+      */
 }
