3387934c593dd12ed1f3e807f44b359605f1f2c8
Nikolai Sazonov <113256059+NikkSazz@users.noreply.github.com>
2025-03-17 12:41:51 -0400

    Add files via upload

---
+++ 2015/day3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventsOfCodes._2015
+{
+    public class Three
+    {
+        public static void Solution(string input)
+        {
+            Console.WriteLine("Day 3 of 2015");
+        }
+    }
+}
---
+++ 2015/main2015.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AdventsOfCodes._2015
+{
+    internal class main2015
+    {
+        public static void askDay()
+        {
+            Console.WriteLine("2015 Advent of Code :)");
+            int dayRequest = 3;
+            switch (dayRequest)
+            {
+                case 3:
+                    string[] threeInput = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"C:\Users\nsazo\source\repos\Advent2024Desktop\Advent2024Desktop\2015\3.txt"));
+                    Three.Solution(threeInput[0]);
+                    break;
+                default:
+                    Console.WriteLine("Incorrect Day Request");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
