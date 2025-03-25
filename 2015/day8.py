512bdbd9ac2fe567f82d13cb4e2ca82754e7a3c6
Nikolai Sazonov <nsazonov9@gmail.com>
2025-03-25 14:53:49 -0400

    8

--- 2015/day8.py
+++ 2015/day8.py
@@ -7,17 +7,12 @@ start_time = time.time()
 with open('2015/Inputs/8.txt', 'r') as file:
     strings = [l.strip() for l in file]
 
-# Test
-strings = {
-    "\"\"",
-    "\"abc\"",
-    "\"aaa\\\"aaa\"",
-    "\"\\x27\""
-}
-
 def convertToASCII(s, i):
-    ascii = bytes.fromhex(s[i+2:i+4]).decode('ascii')
-    print(f"converted {s[i+1:i+4]} to ascii {ascii}")
+    #print(s[i+1:i+4])
+    hexVal = s[i+2:i+4].upper()
+    #print(hexVal)
+    ascii = bytes.fromhex(hexVal).decode('latin-1')
+    #print(f"converted {s[i+1:i+4]} to ascii {ascii}")
     return ascii
 
 
@@ -38,27 +33,42 @@ def memory(s):
                 mem += '"'
                 i+=2
                 continue
-        else: # Regular character 
-            # print(f"Adding {s[i]}")
-            mem += s[i]
+            elif len(s) > i + 1 and s[i+1] == '\\': # \"
+                mem += "\\"
+                i+=2
+                continue
+        mem += s[i]
         i+=1
         # print(s[i])
-    print(f"mem -> {mem}")
+    #print(f"mem -> {mem}")
     return mem
+
+def encodeLen(s):
+    count = 2 # outer ""
+    for c in s:
+        if c=='"' or c=='\\':
+            count+=1
+        count+=1
+    #print(f"{len(s)} <- Literal\n{count} <- Extended\n")
+    return count
 
 totalLiteral = 0
 totalMemory = 0
+partTwo = 0
 for string in strings:
     totalLiteral+=len(string)
     print(string)
     thisMem = len(memory(string))
-    print(f"{len(string)} <- Literal\n{thisMem} <- Memory\n")
+    #print(f"{len(string)} <- Literal\n{thisMem} <- Memory\n")
     totalMemory+=thisMem
+    # pt2
+    partTwo+=encodeLen(string)
+    partTwo-=len(string)
 
 partOne = totalLiteral - totalMemory
 
 print(f"\nPart One: {partOne}")
-print(f"Part Two: {-1}")
+print(f"Part Two: {partTwo}")
 
 elapsed_time = time.time() - start_time
 print(f"Elapsed time: {elapsed_time:.2f} seconds\n")
