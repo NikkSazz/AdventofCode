43aa3652ca06eda02d1b6803aaa887f2e51dab74
Nikolai Sazonov <nsazonov9@gmail.com>
2025-03-25 14:30:11 -0400

    test

--- 2015/day8.py
+++ 2015/day8.py
@@ -9,33 +9,37 @@ with open('2015/Inputs/8.txt', 'r') as file:
 
 # Test
 strings = {
-    #"\"\"",
-    #"\"abc\"",
-    #"\"aaa\\\"aaa\"",
+    "\"\"",
+    "\"abc\"",
+    "\"aaa\\\"aaa\"",
     "\"\\x27\""
 }
 
 def convertToASCII(s, i):
-    print(f"converted {s[i+1:i+4]} to ascii")
-    return 'J'
+    ascii = bytes.fromhex(s[i+2:i+4]).decode('ascii')
+    print(f"converted {s[i+1:i+4]} to ascii {ascii}")
+    return ascii
+
 
 def memory(s):
     mem = ""
     i = 0
     while i < len(s):
-        print(i)
-        if s[i] == '"':
+        if s[i] == '"': # start or end of string, not \"
             i+=1
             continue
         elif s[i] == '\\':
-            if len(s) > i + 3 and s[i+1] == 'x':
+            if len(s) > i + 3 and s[i+1] == 'x': # hex \xAB
                 char = convertToASCII(s,i)
                 i+=4 # [\xAB]
                 mem+=char
-                print(f"{s[i]}, {i}, {i-4}")
                 continue
-        else:
-            print(f"Adding {s[i]}")
+            elif len(s) > i + 1 and s[i+1] == '"': # \"
+                mem += '"'
+                i+=2
+                continue
+        else: # Regular character 
+            # print(f"Adding {s[i]}")
             mem += s[i]
         i+=1
         # print(s[i])
@@ -45,10 +49,11 @@ def memory(s):
 totalLiteral = 0
 totalMemory = 0
 for string in strings:
-    totalLiteral += len(string)
+    totalLiteral+=len(string)
     print(string)
-    thisLen = len(memory(string))
-    print(f"{len(string)} <- Literal\n{thisLen} <- Memory\n")
+    thisMem = len(memory(string))
+    print(f"{len(string)} <- Literal\n{thisMem} <- Memory\n")
+    totalMemory+=thisMem
 
 partOne = totalLiteral - totalMemory
 
