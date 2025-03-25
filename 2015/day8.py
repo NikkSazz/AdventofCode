0c4e467438da8446e981ccc0d643c2e31758e96d
Nikolai Sazonov <113256059+NikkSazz@users.noreply.github.com>
2025-03-25 13:13:42 -0400

    Create day8.py

---
+++ 2015/day8.py
@@ -0,0 +1,30 @@
+import time
+
+print("\nAdvent of Code 2015, Day EIGHT\n")
+
+start_time = time.time()
+
+with open('2015/8.txt', 'r') as file:
+    strings = [l.strip() for l in file]
+
+# Test
+strings = {
+    "\"\"",
+    "\"abc\"",
+    "\"aaa\\\"aaa\"",
+    "\"\\x27\""
+}
+
+totalLiteral = 0
+totalMemory = 0
+for string in strings:
+    totalLiteral += len(string)
+    print(f"{len(string)}\t{string}")
+
+partOne = totalLiteral - totalMemory
+
+print(f"\nPart One: {partOne}")
+print(f"Part Two: {-1}")
+
+elapsed_time = time.time() - start_time
+print(f"Elapsed time: {elapsed_time:.2f} seconds\n")
