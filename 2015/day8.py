6673fa7b146171112685d6a08ab5ab4aef57f980
Nikolai Sazonov <nsazonov9@gmail.com>
2025-03-25 14:10:58 -0400

    getting hex to work

--- 2015/day8.py
+++ 2015/day8.py
@@ -9,22 +9,45 @@ with open('2015/Inputs/8.txt', 'r') as file:
 
 # Test
 strings = {
-    "\"\"",
-    "\"abc\"",
-    "\"aaa\\\"aaa\"",
+    #"\"\"",
+    #"\"abc\"",
+    #"\"aaa\\\"aaa\"",
     "\"\\x27\""
 }
 
+def convertToASCII(s, i):
+    print(f"converted {s[i+1:i+4]} to ascii")
+    return 'J'
 
-def memoryLen(s):
-    return 1
+def memory(s):
+    mem = ""
+    i = 0
+    while i < len(s):
+        print(i)
+        if s[i] == '"':
+            i+=1
+            continue
+        elif s[i] == '\\':
+            if len(s) > i + 3 and s[i+1] == 'x':
+                char = convertToASCII(s,i)
+                i+=4 # [\xAB]
+                mem+=char
+                print(f"{s[i]}, {i}, {i-4}")
+                continue
+        else:
+            print(f"Adding {s[i]}")
+            mem += s[i]
+        i+=1
+        # print(s[i])
+    print(f"mem -> {mem}")
+    return mem
 
 totalLiteral = 0
 totalMemory = 0
 for string in strings:
     totalLiteral += len(string)
     print(string)
-    thisLen = memoryLen(string)
+    thisLen = len(memory(string))
     print(f"{len(string)} <- Literal\n{thisLen} <- Memory\n")
 
 partOne = totalLiteral - totalMemory
