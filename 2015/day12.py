e131689372dc91c3179f786b32315789432869a8
Nikolai Sazonov <nsazonov9@gmail.com>
2025-03-26 10:01:09 -0400

    12pt1

--- 2015/day12.py
+++ 2015/day12.py
@@ -1,11 +1,21 @@
 import time
+import re # regex
 
 print("\nAdvent of Code 2015, Day TWELVE\n")
 
 start_time = time.time()
 
-partOne = 0
-partTwo = 0
+with open('2015/Inputs/12.txt', 'r') as file:
+    lines = file.readlines()
+
+regex = r"-?\d+" # any digit, could be multiple or negative
+numbers = [int(match) for match in re.findall(regex, lines[0])]
+
+def WithinRed():
+    return 50
+
+partOne = sum(numbers)
+partTwo = partOne - WithinRed()
 
 print(f"\nPart One: {partOne}")
 print(f"Part Two: {partTwo}")
